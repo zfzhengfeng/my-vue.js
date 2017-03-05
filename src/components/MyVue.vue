509de5411a020ddd03406730219cfaddfc757e99
郑峰 <zhengfeng@iisun.net>
2017-03-05 12:50:09 +0800

    复习vue.js第二天（补）

---
+++ src/components/MyVue.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test">
+    <div>
+      {{helloVue}}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    data () {
+      return {
+        helloVue: '你好我的vue'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
